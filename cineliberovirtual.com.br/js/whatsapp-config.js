7a3e7a062b763c95c3e6be7533209aca4e050b0d
Nolido7 <145303487+Nolido7@users.noreply.github.com>
2025-10-06 15:54:34 -0300

    Update whatsapp-config.js

--- cineliberovirtual.com.br/js/whatsapp-config.js
+++ cineliberovirtual.com.br/js/whatsapp-config.js
@@ -6,8 +6,8 @@
  */
 
 // CONFIGURAÇÕES - ALTERE APENAS AQUI
-const PHONE_NUMBER = '5521967827833'; // Número com código do país (sem + no início)
-const DEFAULT_MESSAGE = 'Ol%C3%A1%2C+gostaria+de+experimentar+a+Viu+TV+Plus.'; // Mensagem URL encoded
+const PHONE_NUMBER = '556281215133'; // Número com código do país (sem + no início)
+const DEFAULT_MESSAGE = '*Ol%C3%A1%2C+gostaria+de+experimentar+o+Meu+IPTV+Brasil.*'; // Mensagem URL encoded
 
 // LINK GERADO AUTOMATICAMENTE - NÃO ALTERE
 const WHATSAPP_LINK = `https://api.whatsapp.com/send/?phone=${PHONE_NUMBER}&text=${DEFAULT_MESSAGE}&type=phone_number&app_absent=0`;
@@ -18,5 +18,6 @@ window.WHATSAPP_CONFIG = {
     DEFAULT_MESSAGE: DEFAULT_MESSAGE,
     WHATSAPP_LINK: WHATSAPP_LINK
 };
+
 
 console.log('WhatsApp configurado:', WHATSAPP_LINK);
